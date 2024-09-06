e9277e3669844f94f40d03bc4117eff3048306ab
Stefan Weinmann <s.weinmann@intex-online.de>
2024-09-06 15:46:27 +0200

    feat: secret constructor

--- secret/unmarshall.go
+++ secret/unmarshall.go
@@ -29,7 +29,12 @@ func (k *Secret) UnmarshalYAML(data *yaml.Node) (err error) {
 	return
 }
 
-func New(raw string) (secret *Secret, err error) {
+func New(raw string) (secret *Secret) {
+	sec := Secret(raw)
+	return &sec
+}
+
+func FromString(raw string) (secret *Secret, err error) {
 	if sec, err := newSecret(raw); err != nil {
 		return nil, err
 	} else {
